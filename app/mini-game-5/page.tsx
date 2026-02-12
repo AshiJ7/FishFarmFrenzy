dc865f6061af6e0f72cc214c04eddab6a78d7977
Gabriella Smith <msgabyrsmith@gmail.com>
2026-02-12 14:25:54 -0500

    initial setup of minigame 5

--- app/mini-game-5/page.tsx
+++ app/mini-game-5/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import FlipCard from "./flipcard";
 export default function MiniGame5() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -8,6 +8,15 @@ export default function MiniGame5() {
         <p className="mt-4 max-w-2xl text-center text-lg text-zinc-600 dark:text-zinc-400">
           Placeholder for mini-game 5.
         </p>
+        <FlipCard
+            front={<span>How much money does typical farming make?</span>}
+            back={<span>Will fill in with accurate data</span>}
+          />
+
+          <FlipCard
+            front={<span>How much time does traditional farming take to produce lettuce?</span>}
+            back={<span>Will fill in with accurate data</span>}
+          />
         <nav className="mt-8 flex gap-4">
           <Link href="/" className="text-foreground">Home</Link>
           <Link href="/mini-game-4" className="text-foreground">Prev</Link>
